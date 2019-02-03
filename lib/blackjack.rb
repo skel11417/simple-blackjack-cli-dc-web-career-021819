f3364eb5e31402e61249553109eb48591e984a2e
Sean Kelly <shonkelli@gmail.com>
2019-02-03 18:07:40 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -40,6 +40,7 @@ def hit?(card_total)
     user_input = get_user_input
     if user_input == 'h'
       card_total += deal_card
+      display_card_total(card_total)
     elsif user_input == 's'
       break
     else
